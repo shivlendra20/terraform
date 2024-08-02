19e027d4caa3e7670dbf918a9162697225bc1a1d
Shivlendra Singh Jadoun <shivlendra14@gmail.com>
2024-08-03 01:30:39 +0530

    endpointaddedinprovider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+  endpoints {
+    sts = "https://sts.ap-south-1.amazonaws.com"
+  }
 }
